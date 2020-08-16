4eb57f597d33283ecd1a422f2a62114e9c2bf385
Seen3 <63313585+Seen3@users.noreply.github.com>
2020-08-16 20:32:29 +0530

    Update InstertionSort.js

--- InstertionSort.js
+++ InstertionSort.js
@@ -1,1 +1,17 @@
 //This is insertion sort
+function insSort(arr)
+{
+  let position,value;//making variables
+  for (let i=0;i<arr.length;i++)
+  {
+    value=arr[i]; //taking elements to compare
+    position=i;   
+    while(position>0 && arr[position-1]>value) //comparing
+    {
+      arr[position]=arr[position-1]; //swap
+      position=position-1;           //check for elements before it
+    }
+    arr[position]=value; //replacing the value
+  }
+  return arr;
+}
